1b0f6dd55eaaba3945287fee8f6f9124312bf34c
savio0112 <saviocarvalho086@gmail.com>
2019-06-06 10:11:35 -0300

    Parque do gato

---
+++ ExercicioDoGato/src/exerciciodogato/Parque.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciodogato;
+
+
+public class Parque{
+    public static int main (String[]args){
+        Gato filho=new Gato("gota");
+            filho.miar();
+            Gato ola = new Gato ("chuva");
+            ola.miar();
+            
+        return 0;
+    }
+}
