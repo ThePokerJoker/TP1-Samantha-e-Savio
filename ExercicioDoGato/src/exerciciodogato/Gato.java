956e51719558de531134c8da3aa85a4c6430fc1d
ValenteSamantha <svalcacio@gmail.com>
2019-06-06 09:07:59 -0300

    Criei a pasta "Gato", fiz uma public miar

---
+++ ExercicioDoGato/src/exerciciodogato/Gato.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciodogato;
+
+/**
+ *
+ * @author aluno
+ */
+public class Gato {
+
+    boolean raça;
+    String nome;
+    int idade;
+    char sexo;
+
+    public miar() {
+        if (idade<=1){
+            System.out.print("Meow!");
+        }else{
+            System.out.print("Miu!");
+        }
+    }
+
+    
+}
